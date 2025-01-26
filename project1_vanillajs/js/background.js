4386a7822231022ccee116d09cf924f75fda70a7
hyosukjung <hyosukjung32@gmail.com>
2025-01-27 01:12:30 +0900

    폴더구조 변경 후 커밋

--- project1_vanillajs/js/background.js
+++
@@ -1,7 +0,0 @@
-const images=["bg1.JGP","bg2.JGP","bg3.JGP"];
-
-const chosenImage=images[Math.floor(Math.random()*images.length)];//랜덤으로 이미지를 선택
-const bgImage=document.createElement("img");//이미지 태그 생성
-bgImage.src=`images/${chosenImage}`;//이미지 태그의 src속성에 이미지 경로를 넣는다.
-bgImage.classList.add("bg_img");//이미지 태그에 클래스를 추가한다.
-document.body.appendChild(bgImage);//body태그에 이미지태그를 추가한다.
